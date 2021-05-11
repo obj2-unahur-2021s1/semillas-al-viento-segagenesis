46415babd29d9d52af7bde4c131f69ec8a0dea16
AlejandroUnahur <v.alejandrohugo@gmail.com>
2021-05-11 01:35:02 -0300

    Criticado el codigo segun las cualidades de diseño en Planta.kt

--- src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Planta.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Planta.kt
@@ -3,9 +3,11 @@ package ar.edu.unahur.obj2.semillasAlViento
 abstract class Planta(val anioObtencionSemilla: Int, var altura: Float) {
   fun esFuerte() = this.horasDeSolQueTolera() > 10
 
+  // Cohesion: aunque tenga algo que ver con la clase planta esta funcion
+  // no esta relacionada con la misma sino con la clase Parcela
+  // esta funcion iria en la clase Parcela , en la consigna se pide consultarle al objeto Parcela no al objeto Planta
   fun parcelaTieneComplicaciones(parcela: Parcela) =
     parcela.plantas.any { it.horasDeSolQueTolera() < parcela.horasSolPorDia }
-// esta funcion iria en la clase Parcela , en la consigna se pide consultarle al bojeto Parcela no al objeto Planta
 
   abstract fun horasDeSolQueTolera(): Int
   abstract fun daSemillas(): Boolean
@@ -16,13 +18,13 @@ class Menta(anioObtencionSemilla: Int, altura: Float) : Planta(anioObtencionSemi
   override fun daSemillas() = this.esFuerte() || altura > 0.4
 }
 
-open class Soja(anioObtencionSemilla: Int, altura: Float, val esTransgenica: Boolean) : Planta(anioObtencionSemilla, altura) {
+class Soja(anioObtencionSemilla: Int, altura: Float, val esTransgenica: Boolean) : Planta(anioObtencionSemilla, altura) {
   override fun horasDeSolQueTolera(): Int  {
     // ¡Magia de Kotlin! El `when` es como un `if` pero más poderoso:
     // evalúa cada línea en orden y devuelve lo que está después de la flecha.
     val horasBase = when {
       altura < 0.5  -> 6
-      altura < 1    -> 7  // Yo usaria un comparador, porque no marca el limite que es entre 0.5 y 1 // nose usar between ale
+      altura < 1    -> 7
       else          -> 9
     }
     return if (esTransgenica) horasBase * 2 else horasBase
@@ -37,10 +39,10 @@ open class Soja(anioObtencionSemilla: Int, altura: Float, val esTransgenica: Boo
 }
 
 // la soja transgenica la pondria en una subclase de soja, poniendo open en soja y generando una nueva clase
-//class SojaTransgenica(anioObtencionSemilla: Int, altura: Float, esTransgenica: Boolean = true):
-//  Soja(anioObtencionSemilla, altura, esTransgenica) {
-//  override fun daSemillas() = false
-//  override fun horasDeSolQueTolera(): Int {
-//    return super.horasDeSolQueTolera() * 2
-//  }
-//}
+// Desacoplamiento : para evitar complejidades se deberia he
+class SojaTransgenica(anioObtencionSemilla: Int, altura: Float, esTransgenica: Boolean = true): Soja(anioObtencionSemilla, altura, esTransgenica) {
+  override fun daSemillas() = false
+  override fun horasDeSolQueTolera(): Int {
+    return super.horasDeSolQueTolera() * 2
+  }
+}
