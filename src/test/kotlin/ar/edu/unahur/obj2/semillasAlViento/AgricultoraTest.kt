3eb24be360d42365a29e31b9c4254dc632ae1453
AlejandroUnahur <v.alejandrohugo@gmail.com>
2021-05-28 00:57:39 -0300

    Se crearon los test para Agricultora

---
+++ src/test/kotlin/ar/edu/unahur/obj2/semillasAlViento/AgricultoraTest.kt
@@ -0,0 +1,19 @@
+package ar.edu.unahur.obj2.semillasAlViento
+
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+class AgricultoraTest: DescribeSpec({
+    describe("Una agricultora") {
+        val parceleSinPlantas = Parcela(50,50,2)
+        val agricultora = Agricultora(mutableListOf(parceleSinPlantas))
+        it("Sus parcelas semilleras, una parcela es semillera si todas sus plantas dan semillas.") {
+            agricultora.parcelasSemilleras().shouldBe(listOf(parceleSinPlantas))
+        }
+        it("Puede plantar estrategicamente una planta") {
+            val plantaMenta = Menta(2021,3.3f)
+            agricultora.plantarEstrategicamente(plantaMenta)
+            parceleSinPlantas.plantas.size.shouldBe(1)
+        }
+    }
+})
